efcb520c9f040564623f40161c48a16b6fc5aca0
Jeremy Berglund <jtberglund@gmail.com>
2018-05-13 12:16:39 -0700

    WIP: use React context in ThemeProvider

--- src/ThemeProvider.jsx
+++ src/ThemeProvider.jsx
@@ -3,27 +3,49 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ThemeEvents from './ThemeEvents';
 import ThemeManager from './ThemeManager';
+import invariant from 'invariant';
 
-export default class ThemeProvider extends React.Component {
-    constructor(props) {
-        super(props);
-
-        ThemeManager.setCurrentTheme(props.theme);
+export default function make({ themes, styleInterface, initialTheme = themes[0] }) {
+    if (themes.length === 0) {
+        invariant('No themes defined in ThemeProvider.make');
+    }
+    if (!styleInterface) {
+        invariant('No style interface supplied to ThemeProvider.make');
     }
 
-    componentWillReceiveProps(nextProps) {
-        // Notify listeners of theme change
-        if (this.props.theme !== nextProps.theme) {
-            ThemeManager.setCurrentTheme(nextProps.theme);
-            ThemeEvents.publish(ON_THEME_CHANGE, ThemeManager.getTheme(nextProps.theme));
-        }
-    }
+    const { Provider, Consumer } = React.createContext({
+        themes,
+        styleInterface,
+        theme: initialTheme
+    });
 
-    render() {
-        return React.Children.only(this.props.children);
-    }
+    return {
+        Provider,
+        Consumer,
+        themed: () => {}
+    };
 }
 
-ThemeProvider.propTypes = {
-    theme: PropTypes.string
-};
+// export default class ThemeProvider extends React.Component {
+//     constructor(props) {
+//         super(props);
+
+//         ThemeManager.setCurrentTheme(props.theme);
+//     }
+
+//     componentWillReceiveProps(nextProps) {
+//         // Notify listeners of theme change
+//         if (this.props.theme !== nextProps.theme) {
+//             ThemeManager.setCurrentTheme(nextProps.theme);
+//             ThemeEvents.publish(ON_THEME_CHANGE, ThemeManager.getTheme(nextProps.theme));
+//         }
+//     }
+
+//     render() {
+//         return React.Children.only(this.props.children);
+//     }
+// }
+
+// ThemeProvider.propTypes = {
+//     theme: PropTypes.string
+// };
